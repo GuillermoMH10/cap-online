ba7ead654130177b522cf30ba65f92954464c039
Viridiana12345 <22693@virtual.utsc.edu.mx>
2026-03-18 21:22:06 -0600

    Fix syntax error in ALLOWED_HOSTS assignment
    Se corrigió la sintaxis

--- cap_online/settings.py
+++ cap_online/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS = ["*"}
+    ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://cap-online.onrender.com",
